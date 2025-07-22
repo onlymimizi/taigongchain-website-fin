222fa32c532bad8e83f100ab0cb64e098dacece2
onlymimizi <2214003943@qq.com>
2025-07-22 18:29:55 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
+  root: '.', // 👈 指定根目录为当前路径
+  publicDir: 'public',
   build: {
     outDir: 'dist',
   },
