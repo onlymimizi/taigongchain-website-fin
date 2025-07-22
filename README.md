6f20ccedab81d2400d703ea77b3b49a5e7cda997
onlymimizi <2214003943@qq.com>
2025-07-22 18:24:55 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# taigongchain-website-fin
+# TaigongChain Web
+
+基于 React + Vite 构建的钓鱼比赛成绩上链系统前端。
---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from './components/Header'
+import Hero from './components/Hero'
+import Dashboard from './components/Dashboard'
+import Footer from './components/Footer'
+
+export default function App() {
+  return (
+    <>
+      <Header />
+      <Hero />
+      <Dashboard />
+      <Footer />
+    </>
+  )
+}
