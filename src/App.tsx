2049694bfba920d300bb7d57e5ed59a1f6538738
onlymimizi <2214003943@qq.com>
2025-07-24 14:30:57 +0800

    Add user authentication feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,35 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+
 import Header from './components/Header'
 import Hero from './components/Hero'
 import Dashboard from './components/Dashboard'
 import Footer from './components/Footer'
 
+import SignAndQR from './pages/SignAndQR'
+import AuditLogs from './pages/admin/AuditLogs'
+
 export default function App() {
   return (
-    <>
-      <Header />
-      <Hero />
-      <Dashboard />
-      <Footer />
-    </>
+    <Routes>
+      {/* 首页 */}
+      <Route
+        path="/"
+        element={
+          <>
+            <Header />
+            <Hero />
+            <Dashboard />
+            <Footer />
+          </>
+        }
+      />
+
+      {/* 新增页面：签名 & 二维码 */}
+      <Route path="/signqr" element={<SignAndQR />} />
+
+      {/* 新增页面：管理员日志 */}
+      <Route path="/admin/audit-logs" element={<AuditLogs />} />
+    </Routes>
   )
 }
